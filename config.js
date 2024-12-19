d3620cbe24071d38efac03bad084d08de091cdd6
Stephenoladokun <ytstephen111@gmail.com>
2024-12-19 01:44:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ global.waPresence= process.env.WAPRESENCE ||  "null" ; // 'unavailable' | 'avail
 //========================= [ AUTO READ MSGS & CMDS ] =========================\\
 global.readcmds = process.env.READ_COMMAND || "false"
 global.readmessage = process.env.READ_MESSAGE || "false"
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,923xxxxxxxx";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,2349129495184";
 
 
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
